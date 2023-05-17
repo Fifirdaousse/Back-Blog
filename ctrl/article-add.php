a4cefe7511a74c1a56ef63895a04d8b9be44d75e
FC <daousse.fi@gmail.com>
2023-05-17 19:13:23 +0200

    ajout article fonctionnel

---
+++ ctrl/article-add.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['idUtilisateur'])) {
+    header("Location: login.php"); // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
+    exit();
+}
+
+// Vérifier si le formulaire de création d'article est soumis
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Effectuer la validation des données de l'article (assurez-vous d'adapter cette étape à vos besoins)
+    $titre = $_POST['titre'];
+    $texte = $_POST['texte'];
+    $description = $_POST['description'];
+
+
+    // Vérifier les informations de connexion à la base de données (assurez-vous de remplacer les informations de connexion appropriées)
+    $host = '127.0.0.1';
+    $dbname = '520_fc';
+    $user = 'root';
+    $password = '';
+
+    $conn = new mysqli($host, $user, $password, $dbname);
+    if ($conn->connect_error) {
+        die("La connexion a échoué : " . $conn->connect_error);
+    }
+
+    // Insérer le nouvel article dans la base de données
+    // $idUtilisateur = $_SESSION['idUtilisateur'];
+    // $description = $_SESSION('description');
+
+    // $sql = "INSERT INTO article (idUtilisateur, titre, texte, description, illustration)
+    //         VALUES ('$idUtilisateur', '$titre', '$texte', '$description', '$illustration')";
+
+    // if ($conn->query($sql) === TRUE) {
+    //     $message = "Article créé avec succès.";
+    // } else {
+    //     $message = "Erreur lors de la création de l'article : " . $conn->error;
+    // }
+    // $conn->close();
+
+
+
+    // Préparer la requête d'insertion avec des paramètres pour éviter les failles de sécurité
+    $sql = "INSERT INTO article (idUtilisateur, titre, texte, description, illustration)
+            VALUES (?, ?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("issss", $_SESSION['idUtilisateur'], $titre, $texte, $description, $illustration);
+
+    if ($stmt->execute()) {
+        $message = "Article créé avec succès.";
+    } else {
+        $message = "Erreur lors de la création de l'article : " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+include '../view/article-display.php';
+
---
+++ view/article-display.php
@@ -0,0 +1,28 @@
+<?php include '../view/partial/header.php'?>
+
+
+<h1>Créer un article</h1>
+
+<?php if (isset($message)) { ?>
+    <p><?php echo $message; ?></p>
+<?php } ?>
+
+<form method="POST" action="article-add.php" enctype="multipart/form-data">
+    <label for="titre">Titre :</label><br>
+    <input type="text" id="titre" name="titre" required><br><br>
+
+    <label for="desc">Description :</label><br>
+    <textarea id="desc" name="description" required></textarea>
+
+    <label for="texte">Texte :</label><br>
+    <textarea id="texte" name="texte" rows="8" cols="80" required></textarea><br><br>
+
+    <h2>Illustration :</h2>
+        <label for="fileUpload">Fichier :</label>
+        <input type="file" name="illustration" id="fileUpload">
+
+    <button type="submit">Créer l'article</button>
+</form>
+
+
+<?php include '../view/partial/footer.php'?>
